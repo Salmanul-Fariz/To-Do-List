bef9936c21fbd6a55453fdc1d35d5e41f61e20a9
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-07 17:22:32 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoTaskComponent } from './to-do-task/to-do-task.component';
+import { ProgressTaskComponent } from './progress-task/progress-task.component';
+import { CompleteTaskComponent } from './complete-task/complete-task.component';
+import { TaskComponent } from './to-do-task/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoTaskComponent,
+    ProgressTaskComponent,
+    CompleteTaskComponent,
+    TaskComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/progress-task/progress-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-task',
+  templateUrl: './progress-task.component.html',
+  styleUrls: ['./progress-task.component.css']
+})
+export class ProgressTaskComponent {
+
+}
